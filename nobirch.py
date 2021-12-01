d3bf9f1e045d0b5acb737def2f0ebed90d936655
Dylan Coxshall <63456774+DCoxshall@users.noreply.github.com>
2021-12-01 16:38:19 +0000

    Added slowPrint functionality
    Just quietly testing pull requests before I go and fuck up some open source project by accident.

--- nobirch.py
+++ nobirch.py
@@ -1,1 +1,9 @@
-print("Birch shouldn't be here")
+from time import sleep
+
+def slowPrint(text):
+    for i in text:
+        print(i, end="")
+        time.sleep(0.01)
+        
+slowPrint("Birch shouldn't be here")
+slowPrint("Neither should Dylan, but here I am anyway")
